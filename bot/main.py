d7b9f4f0b3b948365066974901ffd347f840f449
Rodrigo Delduca <rodrigodelduca@gmail.com>
2021-01-09 19:30:14 -0300

    first commit

---
+++ bot/main.py
@@ -0,0 +1,45 @@
+import os
+import http
+
+import requests
+
+from flask import Flask, request
+from werkzeug.wrappers import Response
+
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, Filters, CommandHandler, CallbackContext
+
+session = requests.Session()
+
+def run(update: Update, context: CallbackContext) -> None:
+    payload = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": "execute",
+        "params": {
+            "source": update.message.text.strip("/run")
+        }
+    }
+
+    response = session.post(os.environ["JSON_RPC"], json=payload)
+    if not response.ok:
+        with open("donkey.jpeg", "rb") as f:
+            update.message.reply_photo(photo=f)
+    else:
+        update.message.reply_text(response.json()["result"])
+
+
+bot = Bot(token=os.environ["TOKEN"])
+
+dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0)
+dispatcher.add_handler(CommandHandler("run", run))
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def index() -> Response:
+    dispatcher.process_update(
+        Update.de_json(request.get_json(force=True), bot))
+
+    return "", http.HTTPStatus.NO_CONTENT
