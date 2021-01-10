16144db6add74cf64d068a3fd48cf883dca215af
Rodrigo Delduca <rodrigodelduca@gmail.com>
2021-01-10 10:40:03 -0300

    first commit

---
+++ bot/main.py
@@ -0,0 +1,48 @@
+import os
+import http
+
+import requests
+
+from flask import Flask, request
+from werkzeug.wrappers import Response
+
+from telegram import Bot, Update, ParseMode
+from telegram.ext import Dispatcher, Filters, CommandHandler, CallbackContext
+
+app = Flask(__name__)
+
+session = requests.Session()
+
+
+def run(update: Update, context: CallbackContext) -> None:
+    text = update.message.text.strip("/run")
+    if not text:
+        return
+
+    payload = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": "execute",
+        "params": {"source": text},
+    }
+
+    response = session.post(os.environ["JSON_RPC"], json=payload)
+    response.raise_for_status()
+    result = response.json().get("result")
+
+    if result:
+        update.message.reply_text(result)
+
+
+bot = Bot(token=os.environ["TOKEN"])
+
+dispatcher = Dispatcher(bot=bot, update_queue=None, workers=0)
+dispatcher.add_handler(CommandHandler("run", run))
+
+
+@app.route("/", methods=["POST"])
+def index() -> Response:
+    dispatcher.process_update(
+        Update.de_json(request.get_json(force=True), bot))
+
+    return "", http.HTTPStatus.NO_CONTENT
---
+++ container/main.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import tempfile
+
+from jsonrpc import JSONRPCResponseManager, dispatcher
+from werkzeug.wrappers import Request, Response
+
+
+@dispatcher.add_method
+def execute(source: str) -> dict:
+    os.chdir(tempfile.mkdtemp())
+    with open("main.cpp", "w") as f:
+        f.write(source)
+    subprocess.run(["g++", "-march=native", "-O2", "main.cpp"])
+    result = subprocess.run("./a.out", capture_output=True, timeout=60)
+    limit = 1000
+    return result.stdout[:limit].decode('utf-8')
+
+
+@Request.application
+def application(request: Request) -> Response:
+    response = JSONRPCResponseManager.handle(
+        request.data, dispatcher)
+
+    return Response(response.json, mimetype="application/json")
