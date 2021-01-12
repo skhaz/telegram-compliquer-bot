d05b455cb9cce0ca6810e0468863d5186fe1b905
Rodrigo Delduca <rodrigodelduca@gmail.com>
2021-01-12 17:09:03 -0300

    small code refactor

--- bot/main.py
+++ bot/main.py
@@ -1,34 +1,54 @@
 import os
 import http
+import functools
 
 import requests
 
 from flask import Flask, request
 from werkzeug.wrappers import Response
 
-from telegram import Bot, Update, ParseMode
-from telegram.ext import Dispatcher, Filters, CommandHandler, CallbackContext
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, CallbackContext
 
 app = Flask(__name__)
 
-session = requests.Session()
+
+class JSONRPC:
+    def __init__(self, url):
+        self.url = url
+        self.session = requests.Session()
+
+    def _make_payload(self, method, params):
+        return {"id": 0, "jsonrpc": "2.0", "method": method, "params": params}
+
+    def dispatch(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            payload = self._make_payload(func.__name__, kwargs.copy())
+            response = self.session.post(self.url, json=payload, timeout=60)
+            response.raise_for_status()
+            return response.json()
+
+        return wrapper
+
+
+rpc = JSONRPC(url=os.environ["JSON_RPC"])
+
+
+@rpc.dispatch
+def execute(source: str) -> dict:
+    pass
 
 
 def run(update: Update, context: CallbackContext) -> None:
-    text = update.message.text.strip("/run")
+    message = update.message
+    text = message.text.strip("/run")
     if not text:
         return
 
-    payload = {
-        "id": 0,
-        "jsonrpc": "2.0",
-        "method": "execute",
-        "params": {"source": text},
-    }
+    user = message.from_user or message.chat_id
 
-    response = session.post(os.environ["JSON_RPC"], json=payload, timeout=60)
-    response.raise_for_status()
-    result = response.json().get("result")
+    result = execute(source=text).get("result")
 
     if result:
         message = update.message.reply_to_message or update.message
